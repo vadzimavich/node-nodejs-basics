c7988eaba5f8df1b7080f83301f3e50e58c28c9d
vadzimavich <vadzimavich@gmail.com>
2024-10-06 21:29:47 +0300

    feat: fs - create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { access, writeFile } from 'fs/promises'; 
+import { constants } from 'fs'; 
+
 const create = async () => {
-    // Write your code here 
+  const filePath = './src/fs/files/fresh.txt';
+  try {
+    await access(filePath, constants.F_OK);
+    throw new Error('FS operation failed'); 
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+        await writeFile(filePath, 'I am fresh and young');
+        console.log('File created successfully');
+      } else {
+        throw err;
+      }
+    }
 };
 
 await create();
