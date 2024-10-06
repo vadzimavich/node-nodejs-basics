070d5916f10f9728b594ecac369c9befd21e0866
vadzimavich <vadzimavich@gmail.com>
2024-10-06 23:05:12 +0300

    feat: fs - read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import { access, readFile } from 'fs/promises';
+import { constants } from 'fs';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = './src/fs/files/fileToRead.txt';
+
+  try {
+    await access(filePath, constants.f_OK);
+    const data = await readFile(filePath, 'utf8');
+    console.log(data);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error('FS operation failed. File does not exist');
+    } else {
+      throw err;
+    }
+  }
 };
 
 await read();
