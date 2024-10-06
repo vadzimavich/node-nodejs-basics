24da70928980793fa70f84541993e0e5b88221af
vadzimavich <vadzimavich@gmail.com>
2024-10-06 21:48:42 +0300

    feat: fs - copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { access, cp } from 'fs/promises';
+import { constants } from 'fs';
+
 const copy = async () => {
-    // Write your code here 
+  const srcFolder = './src/fs/files';
+  const destFolder = './src/fs/files_copy';
+
+  try {
+    await access(srcFolder, constants.F_OK);
+    await access(destFolder, constants.F_OK);
+    throw new Error('FS operation failed');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await cp(srcFolder, destFolder, { recursive: true });
+      console.log('Folder copied successfully');
+    } else {
+      throw err;
+    }
+  }
 };
 
 await copy();
