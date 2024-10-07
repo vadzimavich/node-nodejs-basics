e07ce280eef7f193e60ae77309c0a2aebcec6622
vadzimavich <vadzimavich@gmail.com>
2024-10-07 16:14:24 +0300

    feat: wt - main.js & worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
-};
+  const coresNum = os.cpus().length;
+  const results = [];
+    
+  const createWorker = (index) => {
+    return new Promise((resolve) => {
+      const worker = new Worker('./src/wt/worker.js');
+
+      worker.postMessage(10 + index);
+
+      worker.on('message', (result) => {
+        resolve({ status: 'resolved', data: result });
+      });
+      worker.on('error', () => {
+        resolve({ status: 'error', data: null });
+      });
+    });
+  };
+
+  for (let i = 0; i < coresNum; i++) {
+    results.push(createWorker(i));
+  }
+
+  const finalResults = await Promise.all(results);  
+  console.log(finalResults);
+}
 
 await performCalculations();
