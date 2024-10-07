8e631a7c764146701f53593f62df55f56d422eb9
vadzimavich <vadzimavich@gmail.com>
2024-10-07 14:09:28 +0300

    feat: zip - compress.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,11 +1,11 @@
 const parseArgs = () => {
-  const args = process.argv;
+  const args = process.argv.slice(2);
   const result = [];
 
   for (let i = 0; i < args.length; i += 2) {
     const propName = args[i].replace(/^--/, ''); 
     const propValue = args[i + 1];
-    
+
     result.push(`${propName} is ${propValue}`);
   }
 
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+
 const compress = async () => {
-    // Write your code here 
+  const srcFilePath = 'zip/files/fileToCompress.txt';
+  const zipFilePath = 'zip/files/archive.gz';
+  const readStream = createReadStream(srcFilePath);
+  const writeStream = createWriteStream(zipFilePath);
+  const gzip = createGzip();
+
+  pipeline(
+    readStream,
+    gzip,
+    writeStream,
+    (err) => {
+      if (err) {
+        console.error('Error compressing file:', err);
+      } else {
+        console.log('File compressed successfully!');
+      }
+    }
+  );
 };
 
 await compress();
